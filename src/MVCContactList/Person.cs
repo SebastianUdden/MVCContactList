19821ffe588f786e41d08316dc965c3437051879
SebastianUdden <sebastian.udden@gmail.com>
2016-03-01 16:25:37 +0100

    Lade till person class och CPVM

---
+++ src/MVCContactList/Person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCContactList
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+
+        public Person(string name, string email)
+        {
+            Name = name;
+            Email = email;
+        }
+    }
+}
