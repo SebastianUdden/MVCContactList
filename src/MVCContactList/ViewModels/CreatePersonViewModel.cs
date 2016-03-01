e2b2915d363365692935876f3b97e04c4c781f54
SebastianUdden <sebastian.udden@gmail.com>
2016-03-01 16:42:02 +0100

    Lade till CreatePersonView

---
+++ src/MVCContactList/ViewModels/CreatePersonViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCContactList.ViewModels
+{
+    public class CreatePersonViewModel
+    {
+        [Display(Name = "First Name")]
+        [Required(ErrorMessage = "Enter a name.")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Enter an email with an @ sign.")]
+        public string Email { get; set; }
+
+        [Display(Name = "I accept terms and conditions.")]
+        public bool AcceptTerms { get; set; }
+    }
+}
